4c61d9b5d4aabe253df6a4bc6af664a3834cb495
victor <victor@elalemanyo.de>
2022-08-29 10:48:15 +0200

    Update name and author information

--- init.rb
+++ init.rb
@@ -1,8 +1,8 @@
 Redmine::Plugin.register :redmine_event_notifier do
-  name 'Redmine Event Notifier plugin'
-  author 'DotRub GmbH'
+  name 'Redmine Event Notifier'
+  author 'DotRuby GmbH'
   description 'This is a plugin for Redmine'
   version '0.0.1'
-  url 'http://example.com/path/to/plugin'
-  author_url 'http://example.com/about'
+  url 'https://github.com/dotruby/redmine_event_notifier'
+  author_url 'https://www.dotruby.com/'
 end

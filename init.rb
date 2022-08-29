8ca013cd9bbf6559a8e02572a7a869f40f33eab2
Daniel Schoppmann <daniel.schoppmann@gmail.com>
2022-08-29 10:35:17 +0200

    Initial commit

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :redmine_event_notifier do
+  name 'Redmine Event Notifier plugin'
+  author 'DotRub GmbH'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+  url 'http://example.com/path/to/plugin'
+  author_url 'http://example.com/about'
+end

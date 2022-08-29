b758480034dab0f90723d424562187239adb3ac0
victor <victor@elalemanyo.de>
2022-08-29 21:39:58 +0200

    add pagination

--- app/controllers/event_notifications_controller.rb
+++ app/controllers/event_notifications_controller.rb
@@ -3,5 +3,15 @@ class EventNotificationsController < ApplicationController
 
   def index
     @event_notifications = EventNotification.all
+
+    respond_to do |format|
+      format.html do
+        scope = EventNotification.all
+
+        @event_notification_count = scope.count
+        @event_notification_pages = Paginator.new @event_notification_count, per_page_option, params['page']
+        @event_notifications = scope.limit(@event_notification_pages.per_page).offset(@event_notification_pages.offset).to_a
+      end
+    end
   end
 end
